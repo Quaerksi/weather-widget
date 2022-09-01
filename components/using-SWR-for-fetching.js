60185b412a1a24e23bdd77744593ba18161bfcd4
Juliette <juliettesalevsky@gmail.com>
2022-09-01 10:53:19 +0200

    add ? to object query, add .en, add a TO DO

---
+++ components/using-SWR-for-fetching.js
@@ -0,0 +1,21 @@
+import useSWR from 'swr'
+import { useRouter } from 'next/router'
+import { useState } from 'react';
+
+const fetcher = (url) => fetch(url).then((res) => res.json())
+
+export default function Profile() {
+  const router = useRouter()
+  const [location, setLocation] = useState('Berlin')
+  // const [location, setLocation] = useState('bbb')
+
+  // Use a ternary operator to only fetch the data when the ID isn't undefined
+  const { data, error } = useSWR(location ? `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=FK5HXPC57W977BW6Q2732WJHX&contentType=json` : null, fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
+  console.log(data)
+
+  return <div>hello!</div>
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -5,11 +5,16 @@ import useStore from '../lib/store'
 import * as Calculations from '../lib/calculations'
 import styles from '../styles/widget.module.css'
 import layoutStyles from '../components/layout.module.css'
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react'
 
-const FORECAST_DAYS = 5;
+// import * as dotenv from 'dotenv'
+// dotenv.config()
+
+const FORECAST_DAYS = 4;
 
 export default function Widget(props) {
+
+  console.log(process.env.KEY)
 
   // setLocation
   const [validLocation, setValidLocation] = useState(true);
@@ -30,10 +35,8 @@ export default function Widget(props) {
     
   const handleData = location => {
 
-    // key=3C8J5WCQHMGA7K7V8C37E2E52
-    // another key=FK5HXPC57W977BW6Q2732WJHX
     // queries are limited
-    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=3C8J5WCQHMGA7K7V8C37E2E52&contentType=json`).then(res=>{
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${process.env.NEXT_PUBLIC_KEY}&contentType=json`).then(res=>{
     
       if(res.ok){
         setValidLocation(state => true)
@@ -48,20 +51,22 @@ export default function Widget(props) {
       
       if(data.address){
 
+        // To Do: break up in small functions
+
         scrolllToWidget.current.scrollIntoView();
         /***************current Data start********************************************************************** */
 
-        let precipitationTruth = data.currentConditions.precipprob ? Math.round(data.currentConditions.precipprob) : 0
-        let windspeed = data.currentConditions.windspeed ? Calculations.milesToKilometers(data.currentConditions.windspeed) :0
-        let cloudcover = data.currentConditions.cloudcover ? Math.round(data.currentConditions.cloudcover) : 0
+        let precipitationTruth = data?.currentConditions.precipprob ? Math.round(data.currentConditions.precipprob) : 0
+        let windspeed = data?.currentConditions.windspeed ? Calculations.milesToKilometers(data.currentConditions.windspeed) :0
+        let cloudcover = data?.currentConditions.cloudcover ? Math.round(data.currentConditions.cloudcover) : 0
 
         addCurrent({
-          resolvedLocation: data.resolvedAddress,
-          temp: Calculations.FahrenheitToDegree(data.currentConditions.temp),
-          humidity: Math.round(data.currentConditions.humidity),
+          resolvedLocation: data?.resolvedAddress,
+          temp: Calculations.FahrenheitToDegree(data?.currentConditions.temp),
+          humidity: Math.round(data?.currentConditions.humidity),
           precipitationTruth: precipitationTruth,
           windSpeed: windspeed,
-          conditions: data.currentConditions.conditions,
+          conditions: data?.currentConditions.conditions,
           cloudcover: cloudcover
         })
 
@@ -74,20 +79,20 @@ export default function Widget(props) {
         for(let i = 0; i <= FORECAST_DAYS; i++){
 
           let day = ''
-          const tempMax = Calculations.FahrenheitToDegree(data.days[i].tempmax)
-          const tempMin = Calculations.FahrenheitToDegree(data.days[i].tempmin)
-          let precipitationTruth = data.days[i].precipprob ? Math.round(data.days[i].precipprob) : 0
-          let cloudcover = data.days[i].cloudcover ? Math.round(data.days[i].cloudcover) : 0
+          const tempMax = Calculations?.FahrenheitToDegree(data?.days[i].tempmax)
+          const tempMin = Calculations?.FahrenheitToDegree(data?.days[i].tempmin)
+          let precipitationTruth = data?.days[i].precipprob ? Math.round(data?.days[i].precipprob) : 0
+          let cloudcover = data?.days[i].cloudcover ? Math.round(data?.days[i].cloudcover) : 0
 
           if(i == 0){
 
             day = 'today'
-            addForecast({day: day, precipitationTruth: precipitationTruth, cloudcover: cloudcover, conditions: data.days[i].conditions, tempmax: tempMax, tempmin: tempMin})
+            addForecast({day: day, precipitationTruth: precipitationTruth, cloudcover: cloudcover, conditions: data?.days[i].conditions, tempmax: tempMax, tempmin: tempMin})
           }
           else{
             
             day = Calculations.getWeekday(data.days[i].datetime)
-            addForecast({day: day,  precipitationTruth: precipitationTruth, cloudcover: cloudcover, conditions: data.days[i].conditions, tempmax: tempMax, tempmin: tempMin})
+            addForecast({day: day,  precipitationTruth: precipitationTruth, cloudcover: cloudcover, conditions: data?.days[i].conditions, tempmax: tempMax, tempmin: tempMin})
           }
         }
           
@@ -118,7 +123,7 @@ export default function Widget(props) {
   } 
 
   useEffect(() => {
-    handleData('Berlin');
+    // handleData('Berlin');
   }, []);
 
   return (
@@ -132,7 +137,7 @@ export default function Widget(props) {
           </div>
       </form>  
       <div className={`${styles.resolvedAddress}`}>
-        {!validLocation && <p  style={{color: "red"}}>Please Enter a valid Location</p>}
+        {!validLocation && <p  style={{color: "red"}}>Please enter a valid location</p>}
       </div>
       <div ref={scrolllToWidget} className={`${styles.widget}`}>
           <div className={`${styles.currentConditionsHeader}`}>
